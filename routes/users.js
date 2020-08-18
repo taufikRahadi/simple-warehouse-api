4a67fbcc9cd2620998d39e8950d455eea9c2edde
taufikRahadi <taufikrahadi1@gmail.com>
2020-08-18 09:44:31 +0700

    change root user route

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
+const UserController = require('../controllers/user-controller')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', UserController.index);
 
 module.exports = router;
