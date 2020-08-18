ad69a738e3102eb02f0bf2e4fb1e24263f1f736b
taufikRahadi <taufikrahadi1@gmail.com>
2020-08-18 13:47:41 +0700

    define /in for product in

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const productRouter = require('./routes/products')
+const productInRouter = require('./routes/productin')
 
 var app = express();
 
@@ -18,5 +19,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/products', productRouter)
+app.use('/in', productInRouter)
 
 module.exports = app;
