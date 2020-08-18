15feb68844d1fa93c073a9516bb3b816bf60d61f
taufikRahadi <taufikrahadi1@gmail.com>
2020-08-18 15:02:45 +0700

    define association

--- models/productout.js
+++ models/productout.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      ProductOut.belongsTo(models.Product, { as: 'product', foreignKey: 'productId' })
     }
   };
   ProductOut.init({
