535757bd281fb7856d41fae43628434941e81dcc
taufikRahadi <taufikrahadi1@gmail.com>
2020-08-18 12:57:53 +0700

    generate producOut migration & model

---
+++ models/productout.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class ProductOut extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  ProductOut.init({
+    date: DataTypes.DATE,
+    total: DataTypes.INTEGER,
+    productId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'ProductOut',
+    tableName: 'ProductOut'
+  });
+  return ProductOut;
+};
