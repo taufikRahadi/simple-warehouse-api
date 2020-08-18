511ffe8e0cf949543ea8da83e4080dbd49d8af16
taufikRahadi <taufikrahadi1@gmail.com>
2020-08-18 13:46:34 +0700

    model/productin.js: add association

--- models/productin.js
+++ models/productin.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      ProductIn.belongsTo(models.Product, { as: 'product', foreignKey: 'productId' })
     }
   };
   ProductIn.init({
