af0160c130a126c9eddff5c7212a14f18da89936
taufikRahadi <taufikrahadi1@gmail.com>
2020-08-18 12:38:51 +0700

    product route

--- routes/products.js
+++ routes/products.js
@@ -5,4 +5,7 @@ const cloudinary = require('../middleware/cloudinary')
 router.get('/', ProductController.index)
     .post('/', cloudinary.single('photo'), ProductController.store)
 
+router.patch('/:id', cloudinary.single('photo'), ProductController.update)
+    .get('/:id', ProductController.show)
+    .delete('/:id', ProductController.destroy)
 module.exports = router
