017f182bf0b14498c79879ce4dc3dbca761c48bf
taufikRahadi <taufikrahadi1@gmail.com>
2020-08-18 12:38:44 +0700

    product crud complete

--- controllers/product-controller.js
+++ controllers/product-controller.js
@@ -32,7 +32,59 @@ class ProductController {
         try {
             if(req.file) req.body.photo = req.file.path
             const product = await Product.create({ ...req.body })
-            res.status(500).json(response('succcess', 'product created', product))
+            res.status(201).json(response('succcess', 'product created', product))
+        } catch (error) {
+            res.status(500).json(response('fail', error.message))
+        }
+    }
+
+    static async update (req, res) {
+        try {
+            const product = await Product.findByPk(req.params.id)
+            if(req.file) { 
+                req.body.photo = req.file.path
+                product.photo = req.body.photo
+            }
+            const { name, stock, price, userId } = req.body
+            if (!product) return res.status(404).json(response('success', 'product not found'))
+            product.name = name
+            product.stock = stock
+            product.price = price
+            product.userId = userId
+            await product.save()
+            res.status(200).json(response('success', 'product update', product))
+        } catch (error) {
+            res.status(500).json(response('fail', error.message))
+        }
+    }
+
+    static async show (req, res) {
+        try {
+            const product = await Product.findOne({
+                where: {
+                    id: req.params.id
+                },
+                include: [
+                    { model: User, as: 'supplier' }
+                ]
+            })
+
+            if(!product) return res.status(404).json(response('fail', 'product not found'))
+            res.status(200).json(response('success', 'get product by id', product))
+        } catch (error) {
+            res.status(500).json(response('fail', error.message))
+        }
+    }
+
+    static async destroy (req, res) {
+        try {
+            const product = await Product.findByPk(req.params.id)
+            await Product.destroy({
+                where: {
+                    id: req.params.id
+                }
+            })
+            res.status(200).json(response('success', 'product deleted'))
         } catch (error) {
             res.status(500).json(response('fail', error.message))
         }
