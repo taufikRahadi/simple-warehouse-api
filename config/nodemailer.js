1ac99e709b06a3bcc1b979270d6978618c114503
taufikRahadi <taufikrahadi1@gmail.com>
2020-08-19 07:15:28 +0700

    change auth from process env => using plain text

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 module.exports = {
     service: process.env.EMAIL_SERVICE,
     auth: {
-        user: process.env.AUTH_EMAIL_USER,
-        pass: process.env.AUTH_EMAIL_PASSWORD
+        user: 'taufikrhdi@gmail.com',
+        pass: 'warung325'
     }
 }
